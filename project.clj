832387fbff53dce82eb87c196e1bf6a819496009
Andy Kriger <akriger@jungroup.com>
2012-02-03 17:38:42 -0500

    Added vmfest as git dependency

--- project.clj
+++ project.clj
@@ -6,7 +6,11 @@
                  [clj-http "0.2.7"]
                  [ring-bounce-favicon "0.1.0"]]
   :dev-dependencies [[lein-ring "0.5.4"]
+                     [lein-git-deps "0.0.1-SNAPSHOT"]
                      [clj-stacktrace "0.2.4"]]
+  :git-dependencies [["git://github.com/tbatchelli/vmfest.git" "develop"]]
   :ring {:handler math.core/app}
-  :extra-classpath-dirs [~(str (.toURI (java.io.File. "grinder/lib/*")))]
+  :extra-classpath-dirs [~(str (.toURI (java.io.File. "grinder/lib/*")))
+                         ".lein-git-deps/vmfest/src"
+                         ".lein-git-deps/vmfest/lib/*"]
 )
