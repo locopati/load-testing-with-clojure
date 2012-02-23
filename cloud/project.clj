00761051993f30500d7419490bd1e9ed4755d4ba
Andy Kriger <andy@householder-yogi.net>
2012-02-22 20:51:13 -0500

    Configured cloud project and removed cloud deps from math project

--- cloud/project.clj
+++ cloud/project.clj
@@ -1,3 +1,11 @@
 (defproject cloud "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "Cloud-based agents for load testing"
+  :dependencies [[org.cloudhoist/pallet "0.6.7"]
+                 [org.cloudhoist/pallet-crates-standalone "0.5.0"]
+                 [org.slf4j/slf4j-api "1.6.1"]
+                 [ch.qos.logback/logback-core "1.0.0"]
+                 [ch.qos.logback/logback-classic "1.0.0"]
+                 [org.jclouds/jclouds-all "1.0.0"]
+                 [org.jclouds.driver/jclouds-jsch "1.0.0"]
+                 [org.jclouds.driver/jclouds-slf4j "1.0.0"]]
+  :repositories {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
